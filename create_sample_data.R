43f24f9198085e06bf7a35d60d0e7d60d07cf01a
Yun-Jung <rebecca86420@gmail.com>
2024-04-30 00:20:08 -0400

    Update create_sample_data.R

--- create_sample_data.R
+++ create_sample_data.R
@@ -3,7 +3,7 @@ create_sample_data <- function(folder, output1, output2) {
   # Function to fetch current network statistics using 'ifconfig'
   get_network_stats <- function() {
     system("sudo apt update", intern = TRUE)
-    system("sudo apt install -y net-tools", intern = TRUE)
+    system("sudo apt install -y apt-utils net-tools", intern = TRUE)
     command <- "ifconfig"
     stats_output <- system(command, intern = TRUE)
     parse_network_interface_stats(stats_output)
