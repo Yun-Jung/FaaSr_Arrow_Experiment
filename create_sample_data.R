90fd69e145ee6d89067c68c7740bef47b19edb1e
Yun-Jung <rebecca86420@gmail.com>
2024-04-30 00:00:29 -0400

    Update create_sample_data.R

--- create_sample_data.R
+++ create_sample_data.R
@@ -2,7 +2,7 @@ create_sample_data <- function(folder, output1, output2) {
   ###################### ADD NETWORK MONITORING FUNCTION #####################################
   # Function to fetch current network statistics for all interfaces
   get_all_network_stats <- function() {
-    system("sudo apt-get update")
+    system("sudo apt-get -qq update")
     system("sudo apt-get install -qq -y iproute2-doc iproute2")
     command <- "ip -s link"
     stats_output <- system(command, intern = TRUE)
