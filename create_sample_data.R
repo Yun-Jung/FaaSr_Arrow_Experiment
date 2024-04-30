63e25ad9f9d1599475e3f2a7c23f68cd39305595
Yun-Jung <rebecca86420@gmail.com>
2024-04-30 01:22:27 -0400

    Update create_sample_data.R

--- create_sample_data.R
+++ create_sample_data.R
@@ -5,7 +5,7 @@ use_python("/usr/bin/python3")
 create_sample_data <- function(folder, output1, output2) {
   ###################### ADD NETWORK MONITORING FUNCTION #####################################
   print('test1')
-  system('conda install psutil')
+  py_install("psutil")
   print('test2')
   
   psutil <- import("psutil")
